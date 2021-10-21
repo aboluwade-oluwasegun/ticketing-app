d45327f87d1dd0438cbef59e53680250b403b1cb
Oluwasegun Aboluwade <aboluwade.oluwasegun@gmail.com>
2021-10-21 23:01:19 +0100

    publishers set up

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,54 @@
+import request from 'supertest'
+import { app } from '../../app'
+import { Ticket } from '../../models/ticket' 
+import { OrderStatus, Order } from '../../models/order'
+import { natsWrapper } from '../../nats-wrapper'
+
+it('marks an order as cancelled', async () => {
+    const ticket = await Ticket.build({
+        title: 'concert',
+        price: 20
+    })
+    await ticket.save()
+
+    const user = global.signin()
+
+    const { body: order} =await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({ ticketId: ticket.id})
+        .expect(201)
+
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', user)
+        .send()
+        .expect(204)
+
+    const updatedOrder = await Order.findById(order.id)
+    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
+})
+
+it('emits an order cancelled event', async () => {
+    const ticket = await Ticket.build({
+        title: 'concert',
+        price: 20
+    })
+    await ticket.save()
+
+    const user = global.signin()
+
+    const { body: order} =await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({ ticketId: ticket.id})
+        .expect(201)
+
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', user)
+        .send()
+        .expect(204)
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
